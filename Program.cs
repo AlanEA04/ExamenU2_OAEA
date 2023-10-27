c47adccd7e3d08a013fae26a2da78da31426bb26
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-10-27 07:49:13 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_OAEA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
