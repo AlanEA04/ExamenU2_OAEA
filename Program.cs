48cb10f488288977968331a48f2bf6a2b92294af
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-10-27 09:19:50 -0700

    HOLA

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Examen_OAEA
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Examen_OAEA
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int TotalxProducto;
+            int TotaxVendedor;
+            int suma;
+
+
+            Console.WriteLine("Se muestra una tabla con los productos");
+
+
+
+            int[,] productos = new int[5, 4]
+            {
+                {500, 3000, 100, 400 },
+                {1000, 150, 200, 500 },
+                {250, 1800, 2900, 300 },
+                {400, 130, 90, 2400 },
+                {60, 20, 4000, 3600 }
+
+            };
+            for(int i = 0; i < productos.GetLength(1); i++) 
+            {
+
+
+                //suma = TotalxProducto = productos[i];
+
+
+
+                for (int j = 0; j < productos.GetLength(1); j++)
+                {
+                    Console.WriteLine(productos[i, j] );
+
+                    suma = TotaxVendedor = productos[i, j] / 2;
+
+                    Console.WriteLine(" El total del vendedor es: " +  suma);
+
+                   
+                }
+
+
+
+            }
+            
         }
     }
 }
+
+/*Dada la siguiente tabla de productos por vendedores, cree un programa que utilice
+ * un arreglo bidimensional para calcular la ganancia de la venta por producto y
+ * la ganancia generada por cada vendedor.
+
+Tabla
+
+Incisos:
+
+Imprima la tabla en consola.
+Imprima la ganancia total generada por cada  vendedor. 
+     Ejemplo: "El vendedor 1 tuvo una ganancia de 86 pesos".
+Imprima la ganancia total generada por cada producto. 
+     Ejemplo: "El producto 1 genero 1000 pesos de ganancia."*/
---
+++ tablasProducto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_OAEA
+{
+    internal class tablasProducto
+    {
+
+       
+
+        /* int[,] productos = new int[5, 4]
+         {
+         {500, 300, 100, 400 },
+         {1000, 150, 200, 500 },
+         {250, 1800, 2900, 300 },
+         {400, 130, 90, 2400  },
+         {60, 20, 4000, 3600 }
+         };*/
+
+
+
+
+
+
+
+        /*int[,] Vendedor2 = new int[5, 1]
+             {300, 150 ,1800,130,20};
+
+         int[,] vendedor3 = new int[5, 2]
+             {100, 200, 2900, 90, 4000};
+
+         int[,] Vendedor4 = new int[5, 3]
+              {400, 500, 300, 2400, 3600 };
+         */
+
+    }
+}
